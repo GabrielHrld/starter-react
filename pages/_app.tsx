a5773cbdcf0cf744a3b830524c94f895a9a59d40
Gabi <g.federico.rodriguez@gmail.com>
2021-11-02 15:10:25 -0300

    theme setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 // Redux
@@ -6,12 +5,18 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "../context/reducer/intex";
 
+// Theming
+import { ThemeProvider } from "@mui/material";
+import theme from "../utils/theming/theme";
+
 const store = createStore(reducer);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
---
+++ utils/theming/theme.ts
@@ -0,0 +1,5 @@
+import { Theme } from "@mui/material";
+
+const theme: Partial<Theme> = {};
+
+export default theme;
